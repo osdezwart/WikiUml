7e2efc3509d70424705f4f96a36fdcff2af954c4
Olle de Zwart <olle@endforward.nl>
2011-01-24 23:56:55 +0100

    Added default output path to antlr in build-gramar.bat

--- build-gramar.bat
+++ build-gramar.bat
@@ -1,2 +1,2 @@
-java -classpath d:\Olle\Projects\WikiUml\tools\antlrworks-1.2.2.jar org.antlr.Tool d:\Olle\Projects\WikiUml\source\Grammar\SimpleCalc.g
+java -classpath d:\Olle\Projects\WikiUml\tools\antlrworks-1.2.2.jar org.antlr.Tool -o .\source\WikiUml d:\Olle\Projects\WikiUml\source\Grammar\WikiUml.g
 pause
